1fef0e714d54f31bb2fa8be0d95b832b37a8778d
Rustam Ahmedov <rusmusus@gmail.com>
2018-04-26 17:42:29 -0400

    edited LoginPage

--- src/test/java/com/orangehrm/pages/LoginPage.java
+++ src/test/java/com/orangehrm/pages/LoginPage.java
@@ -1,5 +1,18 @@
 package com.orangehrm.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import com.orangehrm.utilities.Driver;
+
 public class LoginPage {
+	
+	LoginPage(){
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+	
+	public WebElement txtUsername;
+	public WebElement txtPassword;
+	public WebElement btnLogin;
 
 }
